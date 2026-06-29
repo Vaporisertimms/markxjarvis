63ee2266e41f766be6db7332314ad26093fb33e2
STE TIMMS <Stetimms02@gmail.com>
2026-06-29 16:22:04 +0100

    Add SSL fix, startup launcher, and expanded long-term memory

---
+++ launch_jarvis.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "c:\Users\ste\Downloads\Mark-XLVII-main\Mark-XLVII-main"
+python main.py
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+import certifi
+os.environ['SSL_CERT_FILE'] = certifi.where()
+os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+
 import asyncio
 import re
 import threading
